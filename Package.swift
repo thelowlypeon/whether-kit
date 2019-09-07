b8f56c46293898582d2d2cc470a565b5ec3f933c
thelowlypeon <me@petercompernolle.com>
2019-09-07 14:46:14 -0500

    specifics ios 10 as oldest version supported (because of foundation Measurement)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "WhetherKit",
+    platforms: [
+        .ios(.v10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
